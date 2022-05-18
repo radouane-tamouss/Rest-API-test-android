c81be3d24ae486f3c2cbc391a720482889ce1a60
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-18 22:04:26 +0100

    update files

--- app/src/main/java/com/example/datainterventions_testapi/MainActivity.java
+++ app/src/main/java/com/example/datainterventions_testapi/MainActivity.java
@@ -4,7 +4,10 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -32,7 +35,14 @@ public class MainActivity extends AppCompatActivity {
             try {
                 URL url = new URL("http://10.0.2.2/Datainterventions_API/getInterventions.php");
                 URLConnection urlConnection = url.openConnection();
+                InputStreamReader inputStreamReader = new InputStreamReader(urlConnection.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
+                String ligne;
+                while((ligne = bufferedReader.readLine()) != null){
+
+
+                }
             }catch(MalformedURLException e){
                 e.printStackTrace();
             } catch (IOException e) {
